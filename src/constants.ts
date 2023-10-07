f751429fe4737ea1a40ecc6fc823b2df37625fe1
jake-warburton <jakewarburton6@gmail.com>
2023-10-07 01:17:52 +0100

    add comment explaining dotenv issue with movie db url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+//  I wanted to put this in a .env file but I am having this issue when running tests
+//  TS1343: The 'import.meta' meta-property is only allowed when the '--module' option is 'es2020', 'es2022', 'esnext', 'system', 'node16', or 'nodenext'.
 export const MOVIE_DATABASE_URL =
   "https://comforting-starlight-f3456a.netlify.app/.netlify/functions/";
 
